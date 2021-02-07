6f011b0ee6cec8406806c32d6c27aaf9428dcf03
jihadChowdhory <mohammadjihad0333@gmail.com>
2021-02-07 23:20:16 +0600

    last

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-// https://www.themealdb.com/api/json/v1/1/search.php?f=a
-// https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
-// https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
-
 function searchMeal() {
   const mealName = document.getElementById('get-meal-name').value;
   const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=' + mealName;
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      // console.log(data)
       const allMealNames = document.getElementsByClassName('meal-name');
       const allMealImages = document.getElementsByClassName('meal-img');
       const allItems = document.getElementsByClassName('item-div');
@@ -34,6 +29,26 @@ function searchMeal() {
               const mealName = data.meals[0].strMeal;
               document.getElementById('details-meal-img').setAttribute('src', mealImg);
               document.getElementById('details-meal-name').innerText = mealName;
+              const ingredient1 = data.meals[0].strIngredient1;
+              const ingredient2 = data.meals[0].strIngredient2;
+              const ingredient3 = data.meals[0].strIngredient3;
+              const ingredient4 = data.meals[0].strIngredient4;
+              const ingredient5 = data.meals[0].strIngredient5;
+              const ingredient6 = data.meals[0].strIngredient6;
+              const ingredient7 = data.meals[0].strIngredient7;
+              const ingredient8 = data.meals[0].strIngredient8;
+              const ingredient9 = data.meals[0].strIngredient9;
+              const ingredient10 = data.meals[0].strIngredient10;
+              document.getElementById('ingredient1').innerText = ingredient1;
+              document.getElementById('ingredient2').innerText = ingredient2;
+              document.getElementById('ingredient3').innerText = ingredient3;
+              document.getElementById('ingredient4').innerText = ingredient4;
+              document.getElementById('ingredient5').innerText = ingredient5;
+              document.getElementById('ingredient6').innerText = ingredient6;
+              document.getElementById('ingredient7').innerText = ingredient7;
+              document.getElementById('ingredient8').innerText = ingredient8;
+              document.getElementById('ingredient9').innerText = ingredient9;
+              document.getElementById('ingredient10').innerText = ingredient10;
             })
         })
       }
